76d2baad7eb2f20cc7312bc8ccb294bc6c21e136
yui1987 <140272762+yui1987@users.noreply.github.com>
2024-08-13 16:42:58 +0800

    Update customize2.sh

--- customize2.sh
+++ customize2.sh
@@ -54,6 +54,3 @@ git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/l
 # 依赖
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages shadowsocksr-libev
 git_sparse_clone master https://github.com/coolsnowwolf/packages net/shadowsocks-libev
-
-./scripts/feeds update -a
-./scripts/feeds install -a
